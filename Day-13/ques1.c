d994fb3730e277d6bbf69b6a770ef0db8a343d7c
sanyam-code <sanyamjalhotra3@gmail.com>
2025-09-12 10:00:00 +0530

    updated code properly

---
+++ Day-13/ques1.c
@@ -0,0 +1,46 @@
+//Write a program to implement a basic calculator using switch-case for +, -, *, /, %.
+#include <stdio.h>
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    printf("Enter an operator (+, -, *, /, %%): ");
+    scanf(" %c", &operator);
+    printf("Enter two operands: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, result);
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, result);
+            } else {
+                printf("Error! Division by zero.\n");
+            }
+            break;
+        case '%':
+            if ((int)num2 != 0) {
+                result = (int)num1 % (int)num2;
+                printf("%d %% %d = %d\n", (int)num1, (int)num2, (int)result);
+            } else {
+                printf("Error! Division by zero.\n");
+            }
+            break;
+        default:
+            printf("Error! Operator is not correct.\n");
+    }
+
+    return 0;
+}
