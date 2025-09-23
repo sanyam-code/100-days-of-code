8ae50d710cdccf637157bbf35e5c1b48dee36c9f
sanyam-code <sanyamjalhotra3@gmail.com>
2025-09-23 10:00:00 +0530

    updated code properly

---
+++ Day-24/ques1.c
@@ -0,0 +1,20 @@
+/*Write a program to print the following pattern:
+*
+**
+***
+****
+*****
+*/
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter The Number Of Rows");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
